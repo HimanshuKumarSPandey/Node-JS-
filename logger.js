9fea5f9cf88b0e1c997eb033dd40f3b3a925bd7e
HimanshuKumarSPandey <97673386+HimanshuKumarSPandey@users.noreply.github.com>
2022-06-26 08:45:20 +0530

    Creating Module

---
+++ logger.js
@@ -0,0 +1,3 @@
+//Creating Module.
+//this module is going to use one of those remote logging services for login our messages so there are websites out there provide logging as a service they give us a
+//URL and we can send an HTTP request to that URL to log messages in the cloud. 
