f158dda42f7f3d68d4d529f2cfdfc4b8560ae1ca
HimanshuKumarSPandey <97673386+HimanshuKumarSPandey@users.noreply.github.com>
2022-06-27 22:56:11 +0530

    Loading a Module (logger.js file is been loaded)

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-function sayHello(name) {
-  console.log('Hello ' + name);
-}
+//works with logger
 
-sayHello('Hks');
+//declare a variable, call it a logger name of the module and set it to the return value of the require function 
+var logger = require ('./logger');
+
+//let's log this logger
+console.log(logger);
+
+//calling the function (url: 'http://mylogger.org/log') or this method in app.js Output: message 
+logger.log('message');
+
+
+//OutPut
+//{log: ƒ, url: 'http://mylogger.org/log'}
+//Whole thing is an object, this object has a single method called log and has a function. 
