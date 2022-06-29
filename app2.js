7f4593ab57f51eedfc886243cdfd852c919304ec
HimanshuKumarSPandey <97673386+HimanshuKumarSPandey@users.noreply.github.com>
2022-06-29 14:12:17 +0530

    Path Module

--- app2.js
+++ app2.js
@@ -1,7 +1,6 @@
-//jshint is the one of the tool that check our javascript code for any errors. 
+const path = require('path'); //we gonna load the building path module and store it in a constant called path. 
 
-const logger = require('./logger');
+//method used here is parse method. __filename is the argument of module wrapper function
+var pathObj = path.parse(__filename); //parse method with argument is called and stored the result in pathObject. 
 
-logger = 1;
-
-logger.log('message');
+console.log(pathObj); //log it on the console. 
